3a16644b256e4cc5dd0089733939bbabc2131dad
Daniel Franklin <daniel@danielzfranklin.org>
2025-02-04 09:58:33 +0000

    invoke run on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
+IMAGE_TAG=544470575466.dkr.ecr.eu-central-1.amazonaws.com/naptan-map:latest
+REGION=eu-central-1
+FUNCTION=arn:aws:lambda:eu-central-1:544470575466:function:generate-naptan-map
+
 aws ecr get-login-password --region eu-central-1 | \
   docker login --username AWS --password-stdin 544470575466.dkr.ecr.eu-central-1.amazonaws.com
 
-IMAGE_TAG=544470575466.dkr.ecr.eu-central-1.amazonaws.com/naptan-map:latest
 docker buildx build --platform linux/arm64 --provenance=false --push -t "$IMAGE_TAG" .
-aws lambda update-function-code \
-  --region eu-central-1 --function-name generate-naptan-map \
-  --image-uri "$IMAGE_TAG" \
-  --no-cli-pager
+aws lambda update-function-code --region "$REGION" --function-name "$FUNCTION" --image-uri "$IMAGE_TAG" --no-cli-pager
+
+aws lambda invoke  --region "$REGION" --function-name "$FUNCTION" \
+  --invocation-type Event \
+  --no-cli-pager \
+  --payload '{}' /dev/null
